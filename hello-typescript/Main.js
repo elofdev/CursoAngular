9ffd43fd137f6fd11b1e146db9a0f3776f2610ba
elof <ersijunior@gmail.com>
2021-09-17 22:38:30 -0300

    Typescript - exemplos compilação

---
+++ hello-typescript/Main.js
@@ -0,0 +1,40 @@
+//Comparação ts ( Main.ts) para (Main.js)
+//*****************************8 */
+//Javascript puro
+var minhaVar = 'Minha Variável';
+function minhaFuncao(x, y) {
+    return x + y;
+}
+// ES 6 ou ES 2015 - Ecmascript ( alguns browsers não possuem todas as funcionalidades disponíveis)
+// www.es6-features.org#constantes
+// e porisso, precisa de um TransPiller( exemplo o Babel www.babeljs.io) -- compila  em javascript puro vanilla
+//Exemplos
+// let para var
+var num = 3;
+// const para var
+var PI = 3.14;
+// exemplo utilização de arrow functions
+var numeros = [1, 2, 3];
+//Expressão SEM arrow function
+numeros.map(function (valor) {
+    return valor * 2;
+});
+//Expressão COM arrow function
+numeros.map(function (valor) { return valor * 2; }); // ES2015
+//Classe : ES 2015
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+// Possibilidade de Tipar a variável--- informa erro no tipo da variável
+// aceitar qualquet tipo de variável coloca-se........"let n1:any = "  oi deixa sem nada let n1 = 
+//Main.ts:41:1 - error TS2322: Type 'number' is not assignable to type 'string'.
+//41 n1 = 4;
+//Found 1 error.
+//let n1:string = "asdfg";
+var n1 = "asdfg";
+n1 = 4;
